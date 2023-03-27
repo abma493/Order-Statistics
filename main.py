592b55c644c4b3f2b66526bfbffa877ebd65899d
abma493 <abmtz160702@gmail.com>
2023-03-27 16:33:15 -0500

    proto order statistics functions added

--- main.py
+++ main.py
@@ -87,3 +87,4 @@ for i in range(len(Q)):
 	for j in range(len(W)):
 		whataDistances.append(haversine.haversine(Q[i].lat, Q[i].lon, W[j].lat, W[j].lon))
 		print("Query: ", i, W[j], " Distance: ", whataDistances[i+j])
+		
